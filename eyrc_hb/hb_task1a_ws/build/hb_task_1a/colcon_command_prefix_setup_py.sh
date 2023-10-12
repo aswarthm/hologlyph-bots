e1d3cb9d6176efb025bc7ec3823695cabc939799
Aswarth <m.aswarthreddy@gmail.com>
2023-10-12 12:45:20 +0530

    fix gitignore

--- eyrc_hb/hb_task1a_ws/build/hb_task_1a/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/aswarth/eyantra/eyrc_hb/hb_task1a_ws/install/turtlesim/share/turtlesim/package.sh"
--- eyrc_hb/hb_task1b_ws/build/hb_task_1b/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/aswarth/eyantra/eyrc_hb/hb_task1b_ws/install/hb_task_1b'
