7d57bc5a1eb3b229a70059d67492bc2b27a19499
Aswarth <m.aswarthreddy@gmail.com>
2023-10-21 23:40:39 +0530

    modify startup script

--- eyrc_hb/hb_task2/clean_run.sh
+++ eyrc_hb/hb_task2/clean_run.sh
@@ -1,1 +1,4 @@
-rm build/ install/ log/ -r && colcon build --cmake-args -Wno-dev && source install/setup.bash && ros2 launch hb_task2a task2a.launch.py
+rm build/ install/ log/ -r
+colcon build --cmake-args -Wno-dev && 
+source install/setup.bash &&
+ros2 launch hb_task2a task2a.launch.py
